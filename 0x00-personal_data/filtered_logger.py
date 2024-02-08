4f53c9c1e15cbbc5b38490bea8d9ebb952bc1964
ActuaryEmma <emmanyakio@gmail.com>
2024-02-09 01:13:42 +0300

    Task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,12 +4,39 @@ returns the log message obfuscated
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """returns the log message obfuscated"""
-    for field in fields:
-        message = re.sub(
-        rf'{field}=([^{separator}]+)', f'{field}={redaction}', message)
-    return message
+                    message: str, separator: str) -> str:
+        """returns the log message obfuscated"""
+        for field in fields:
+            message = re.sub(
+            rf'{field}=([^{separator}]+)', f'{field}={redaction}', message)
+        return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+    logging.basicConfig(format=FORMAT)
+
+
+    def __init__(self, fields):
+        """init function"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Log Formatter"""
+        log_message = filter_datum(self.fields, self.REDACTION,
+            record.msg, self.SEPARATOR)
+        formatted_log = self.FORMAT % {
+            'name': record.name,'levelname': record.levelname,
+            'asctime': self.formatTime(record),'message': log_message} 
+        return formatted_log
