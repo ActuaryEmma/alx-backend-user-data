e1837328b8cda9d4182c1893ddec93291b638d2c
ActuaryEmma <emmanyakio@gmail.com>
2024-02-08 20:59:39 +0300

    Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,8 @@ returns the log message obfuscated
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     pattern = re.compile(r'password=(\w[a-z]+)|\d{2}\/\d{2}\/\d{4}')
     for mess in message:
