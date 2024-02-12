a570dd54bd58b31bea89989e8ad7d5874aba0eeb
ActuaryEmma <emmanyakio@gmail.com>
2024-02-12 17:59:27 +0300

    Task 3

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+class to manage the API authenctication
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    class to manage the API authenication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method"""
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """public method"""
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ public method"""
+        return None
