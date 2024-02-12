3df021b4d35ce7468b44051edea16ac48bde173f
ActuaryEmma <emmanyakio@gmail.com>
2024-02-12 18:23:33 +0300

    Task 4

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -14,7 +14,14 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """public method"""
-        return False
+        if path is None:
+            return True
+        elif excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for excluded_path in excluded_paths:
+            if path.rstrip('/') == excluded_path.rstrip('/'):
+                return False
+        return True
 
 
     def authorization_header(self, request=None) -> str:
