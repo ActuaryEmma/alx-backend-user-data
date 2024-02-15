e22ee352ba9b2cd678be330bc50cf87de88722a3
ActuaryEmma <emmanyakio@gmail.com>
2024-02-15 21:33:20 +0300

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ class to manage the API authenctication
 
 from flask import request
 from typing import List, TypeVar
+from re import match, sub
 
 
 class Auth:
@@ -19,7 +20,10 @@ class Auth:
         elif excluded_paths is None or len(excluded_paths) == 0:
             return True
         for excluded_path in excluded_paths:
-            if path.rstrip('/') == excluded_path.rstrip('/'):
+            if excluded_path[-1] == '*':
+                sub('*', '.*')
+            if path.rstrip('/') == excluded_path.rstrip('/') or\
+                    match(excluded_path, path):
                 return False
         return True
 
