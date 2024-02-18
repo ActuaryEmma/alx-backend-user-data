a4d1b5a57cbe386e84a71d731cbbc6a9872f7605
ActuaryEmma <emmanyakio@gmail.com>
2024-02-18 17:29:48 +0300

    Task 5 update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -45,9 +45,8 @@ def before_request():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if auth.authorization_header(request) and auth.session_cookie(request):
+    if auth.authorization_header(request) and auth.session_cookie(request) is None:
         abort(401)
-        return None
 
 
 @app.errorhandler(401)
